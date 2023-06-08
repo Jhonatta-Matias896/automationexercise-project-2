813e4b337da4c6c0f4e9dcfd5f26a850af83006d
Jhonatta-Matias896 <131813744+Jhonatta-Matias896@users.noreply.github.com>
2023-06-08 19:07:01 -0300

    refatoração-do-codigo-referente-ao-teste-de-cadastro-e-implementação-do-faker.br

--- cypress/e2e/Ui/cadastro/testesValidosCadastro.cy.js
+++ cypress/e2e/Ui/cadastro/testesValidosCadastro.cy.js
@@ -7,72 +7,72 @@ describe('Teste válidos na tela de cadastro', ()=>{
 beforeEach(() => {
     cy.visit('/login')
 });
-    Cypress._.times(3,()=>{
-        it.only('Teste 01: Cadastro com sucesso', ()=>{
+    Cypress._.times(1,()=>{
+        it('Teste 01: Cadastro com sucesso', ()=>{
             const nomeUsuario = faker.name.firstName()   
             cy.get(elements.campoNome).type(nomeUsuario)
             cy.get(elements.campoEmail).type(faker.internet.email(nomeUsuario))
-            cy.get('[data-qa="signup-button"]').click()
-            cy.contains('Enter Account Information').should('be.visible')
-            cy.get('#id_gender1').click()
-            cy.get('[data-qa="password"]').type(Cypress.env('senhaUsuario'))
-            cy.get('[data-qa="days"]').select('20')
-            cy.get('[data-qa="months"]').select('June')
-            cy.get('[data-qa="years"]').select('2021')
-            cy.get('input[type="checkbox"]').check().should('be.checked')
-            cy.get('[data-qa="first_name"]').type(Cypress.env('usuario01'));
-            cy.get('[data-qa="last_name"]').type(Cypress.env('usuario01'))
-            cy.get('[data-qa="company"]').type('enverga')
-            cy.get('[data-qa="address"]').type('mãe joana')
-            cy.get('[data-qa="country"]').select('Canada')
-            cy.get('[data-qa="state"]').type('CA')
-            cy.get('[data-qa="city"]').type('joana')
-            cy.get('[data-qa="zipcode"]').type('109876543210')
-            cy.get('[data-qa="mobile_number"]').type('012345678910')
-            cy.get('[data-qa="create-account"]').click()
-            cy.contains('Congratulations! Your new account has been successfully created!').should('be.visible')
-            cy.get('[data-qa="continue-button"]').click()
-            cy.get('b').should('have.text', nomeUsuario)
+            cy.get(elements.btnSingUp).click()
+            cy.contains(elements.msgDeCadastro).should('be.visible')
+            cy.get(elements.tituloGenero).click()
+            cy.get(elements.campoSenha).type(Cypress.env('senhaUsuario'))
+            cy.get(elements.diaNascimento).select('20')
+            cy.get(elements.mesNascimento).select('June')
+            cy.get(elements.anoNascimento).select('2021')
+            cy.get(elements.checkBoxe).check().should('be.checked')
+            cy.get(elements.primeiroNome).type(faker.name.firstName());
+            cy.get(elements.ultimoNome).type(faker.name.lastName())
+            cy.get(elements.nomeEmpresa).type(faker.company.companyName())
+            cy.get(elements.nomeRua).type(faker.address.streetName())
+            cy.get(elements.nomePais).select('Canada')
+            cy.get(elements.nomeEstado).type(faker.address.state())
+            cy.get(elements.nomeCidade).type(faker.address.city())
+            cy.get(elements.cep).type(faker.address.zipCode())
+            cy.get(elements.numeroTelefone).type(faker.phone.phoneNumber())
+            cy.get(elements.btnCriarConta).click()
+            cy.contains(elements.msgDeContaCriada).should('be.visible')
+            cy.get(elements.btnContinuar).click()
+            cy.get(elements.verificacaoDeNomeUsuario).should('have.text', nomeUsuario)
                        
        });
 
     })
 
-    it('Teste 01: Cadastro com sucesso', ()=>{
+    it('Teste 02: Cadastro com sucesso(ALTERNATIVO)', ()=>{
         const nomeUsuario = faker.name.firstName()
         faker.internet.email()
 
         cy.get(elements.campoNome).type(nomeUsuario)
         cy.get(elements.campoEmail).type(faker.internet.email(nomeUsuario))
-        cy.get('[data-qa="signup-button"]').click()
-        cy.contains('Enter Account Information').should('be.visible')
-        cy.get('#id_gender1').click()
-        cy.get('[data-qa="password"]').type(Cypress.env('senhaUsuario'))
-        cy.get('[data-qa="days"]').select('20')
-        cy.get('[data-qa="months"]').select('June')
-        cy.get('[data-qa="years"]').select('2021')
-        cy.get('input[type="checkbox"]').check().should('be.checked')
-        cy.get('[data-qa="first_name"]').type(Cypress.env('usuario01'));
-        cy.get('[data-qa="last_name"]').type(Cypress.env('usuario01'))
-        cy.get('[data-qa="company"]').type('enverga')
-        cy.get('[data-qa="address"]').type('mãe joana')
-        cy.get('[data-qa="country"]').select('Canada')
-        cy.get('[data-qa="state"]').type('CA')
-        cy.get('[data-qa="city"]').type('joana')
-        cy.get('[data-qa="zipcode"]').type('109876543210')
-        cy.get('[data-qa="mobile_number"]').type('012345678910')
-        cy.get('[data-qa="create-account"]').click()
-        cy.contains('Congratulations! Your new account has been successfully created!').should('be.visible')
-        cy.get('[data-qa="continue-button"]').click()
-        cy.get('b').should('have.text', nomeUsuario)
+        cy.get(elements.btnSingUp).click()
+        cy.contains(elements.msgDeCadastro).should('be.visible')
+        cy.get(elements.tituloGenero).click()
+        cy.get(elements.campoSenha).type(Cypress.env('senhaUsuario'))
+        cy.get(elements.diaNascimento).select('20')
+        cy.get(elements.mesNascimento).select('June')
+        cy.get(elements.anoNascimento).select('2021')
+        cy.get(elements.checkBoxe).check().should('be.checked')
+        cy.get(elements.primeiroNome).type(Cypress.env('usuario01'));
+        cy.get(elements.ultimoNome).type(Cypress.env('usuario01'))
+        cy.get(elements.nomeEmpresa).type('enverga')
+        cy.get(elements.nomeRua).type('mãe joana')
+        cy.get(elements.nomePais).select('Canada')
+        cy.get(elements.nomeEstado).type('CA')
+        cy.get(elements.nomeCidade).type('joana')
+        cy.get(elements.cep).type('109876543210')
+        cy.get(elements.numeroTelefone).type('012345678910')
+        cy.get(elements.btnCriarConta).click()
+        cy.contains(elements.msgDeContaCriada).should('be.visible')
+        cy.get(elements.btnContinuar).click()
+        cy.get(elements.verificacaoDeNomeUsuario).should('have.text', nomeUsuario)
         
         
    });
 
-   it('Teste 02: Verifica se o cadastro foi concluído com sucesso', () => {
-    cy.get('[data-qa="login-email"]').type(Cypress.env('email01'))
-    cy.get('[data-qa="login-password"]').type(Cypress.env('senhaUsuario'))
-    cy.get('[data-qa="login-button"]').click()
+   it('Teste 03: Verifica se o cadastro foi concluído com sucesso', () => {
+    cy.get(elements.loginCampoEmail).type(Cypress.env('email01'))
+    cy.get(elements.loginCampoSenha).type(Cypress.env('senhaUsuario'))
+    cy.get(elements.btnLogin).click()
     cy.get(':nth-child(10) > a').should('have.visible', Cypress.env('usuario01'))
      });
 
--- cypress/support/Elementos/globalElements.js
+++ cypress/support/Elementos/globalElements.js
@@ -1,6 +1,38 @@
 export const  ELEMENTS = {
     //ELEMENTOS REFERENTES A TELA DE CADASTRO
 campoNome:'[data-qa="signup-name"]',
-campoEmail: '[data-qa="signup-email"]'
+campoEmail: '[data-qa="signup-email"]',
+btnSingUp: '[data-qa="signup-button"]',
+msgDeCadastro: 'Enter Account Information',
+tituloGenero: '#id_gender1',
+campoSenha:'[data-qa="password"]',
+diaNascimento:'[data-qa="days"]',
+mesNascimento: '[data-qa="months"]',
+anoNascimento: '[data-qa="years"]',
+checkBoxe:'input[type="checkbox"]',
+primeiroNome:'[data-qa="first_name"]',
+ultimoNome:'[data-qa="last_name"]',
+nomeEmpresa:'[data-qa="company"]',
+nomeRua:'[data-qa="address"]',
+nomePais:'[data-qa="country"]',
+nomeEstado:'[data-qa="state"]',
+nomeCidade:'[data-qa="city"]',
+cep:'[data-qa="zipcode"]',
+numeroTelefone:'[data-qa="mobile_number"]',
+btnCriarConta:'[data-qa="create-account"]',
+msgDeContaCriada:'Congratulations! Your new account has been successfully created!',
+btnContinuar:'[data-qa="continue-button"]',
+verificacaoDeNomeUsuario:'b',
+
+//ELEMENTOS REFERENES A TELA DE LOGIN
+loginCampoEmail:'[data-qa="login-email"]',
+loginCampoSenha:'[data-qa="login-password"]',
+btnLogin:'[data-qa="login-button"]',
+
+
+
+
+
+
 
 }
